3f1ca330135c41212e0acb17319be78dd4db08e3
Peter Rigii <theonlyrigii@gmail.com>
2023-10-05 16:45:06 +0300

    edited

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
 """Deploy archive!"""
-from fabric.api import local
-from datetime import datetime
+from fabric.api import env, put, run
+import os
+
+env.hosts = ['<IP web-01>', '<IP web-02>']
+env.user = '<username>'
+env.key_filename = '/path/to/ssh/key'
 
 
-def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
+def do_deploy(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+
+    archive_name = os.path.basename(archive_path)
+    archive_root, _ = os.path.splitext(archive_name)
+
     try:
-        current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        file_name = "web_static_" + current_time + ".tgz"
-        local("mkdir -p versions")
-        local("tar -cvzf versions/{} web_static".format(file_name))
-        return "versions/{}".format(file_name)
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_root))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            archive_name, archive_root))
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(
+                archive_root, archive_root))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            archive_root))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(archive_root))
     except Exception as e:
         print(e)
         return None
