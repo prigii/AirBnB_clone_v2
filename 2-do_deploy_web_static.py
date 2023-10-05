59f7ec018432264f86233e9eac97bf45e04a4fc5
Peter Rigii <theonlyrigii@gmail.com>
2023-10-05 16:51:55 +0300

    edited

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,30 +5,23 @@ import os
 
 env.hosts = ['<IP web-01>', '<IP web-02>']
 env.user = '<username>'
-env.key_filename = '/path/to/ssh/key'
+env.key_filename = '/path/to/your/private/key'
 
 
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    archive_name = os.path.basename(archive_path)
-    archive_root, _ = os.path.splitext(archive_name)
+    file_name = archive_path.split('/')[-1]
+    folder_name = file_name.split('.')[0]
 
-    try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_root))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            archive_name, archive_root))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(
-                archive_root, archive_root))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            archive_root))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(archive_root))
-    except Exception as e:
-        print(e)
-        return None
+    put(archive_path, '/tmp/')
+    run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, folder_name))
+    run('rm /tmp/{}'.format(file_name))
+    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder_name, folder_name))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
+    run('rm -rf /data/web_static/current')
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+
+    return True
